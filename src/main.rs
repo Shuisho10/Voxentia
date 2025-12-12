d212ffc5908c74b4abd93772990ff8dbd01c1f8c
Shuisho10 <shuisho.dev@gmail.com>
2025-12-12 21:47:56 +0100

    add: Basic/naive input system.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::collections::HashSet;
+
 use log::*;
+use nalgebra::Vector3;
 use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
+use winit::event::{DeviceEvent, WindowEvent};
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::keyboard::{KeyCode, PhysicalKey};
 use winit::window::WindowId;
 
 use crate::core::engine::VoxelEngine;
@@ -12,6 +16,50 @@ mod vulkan;
 #[derive(Default)]
 struct App {
     pub engine: Option<VoxelEngine>,
+    pub input: InputState,
+}
+
+#[derive(Default)]
+struct InputState {
+    keys_held: HashSet<KeyCode>,
+}
+
+impl InputState {
+    fn is_key_down(&self, code: KeyCode) -> bool {
+        self.keys_held.contains(&code)
+    }
+}
+
+impl App {
+    pub fn handle_input(&mut self) {
+        if let Some(engine) = self.engine.as_mut() {
+            let speed = 20.0;
+            let mut dir = Vector3::zeros();
+
+            if self.input.is_key_down(KeyCode::KeyW) {
+                dir.z += 1.0;
+            }
+            if self.input.is_key_down(KeyCode::KeyS) {
+                dir.z -= 1.0;
+            }
+            if self.input.is_key_down(KeyCode::KeyA) {
+                dir.x -= 1.0;
+            }
+            if self.input.is_key_down(KeyCode::KeyD) {
+                dir.x += 1.0;
+            }
+            if self.input.is_key_down(KeyCode::Space) {
+                dir.y += 1.0;
+            }
+            if self.input.is_key_down(KeyCode::ShiftLeft) {
+                dir.y -= 1.0;
+            }
+
+            if dir.magnitude() > 0.0 {
+                engine.camera.input_move(dir, speed);
+            }
+        }
+    }
 }
 
 impl ApplicationHandler for App {
@@ -19,7 +67,20 @@ impl ApplicationHandler for App {
         let engine = VoxelEngine::new(event_loop).expect("Voxel engine initialization failed");
         self.engine = Some(engine);
     }
-
+    fn device_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _device_id: winit::event::DeviceId,
+        event: DeviceEvent,
+    ) {
+        if let Some(engine) = self.engine.as_mut() {
+            if let DeviceEvent::MouseMotion { delta } = event {
+                engine
+                    .camera
+                    .input_rotate(delta.0 as f32, delta.1 as f32, 0.002);
+            }
+        }
+    }
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
@@ -27,6 +88,7 @@ impl ApplicationHandler for App {
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
+                self.handle_input();
                 self.engine
                     .as_mut()
                     .unwrap()
@@ -47,6 +109,17 @@ impl ApplicationHandler for App {
                         .update_aspect(physical_size.width, physical_size.height);
                 }
                 self.engine.as_ref().unwrap().window.request_redraw();
+            }
+            WindowEvent::KeyboardInput {
+                event: key_event, ..
+            } => {
+                if let PhysicalKey::Code(code) = key_event.physical_key {
+                    if key_event.state.is_pressed() {
+                        self.input.keys_held.insert(code);
+                    } else {
+                        self.input.keys_held.remove(&code);
+                    }
+                }
             }
             _ => (),
         }
